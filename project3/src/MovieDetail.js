ab75c3dbd2928618b58ab1e15d4901d9c8480793
grpargeter <pargetergregr@johndeere.com>
2020-08-21 14:08:54 -0500

    wired up movie detail and list components

---
+++ project3/src/MovieDetail.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function MovieDetail() {
+  return <h2>MovieDetail</h2>;
+}
+
+export default MovieDetail;
---
+++ project3/src/MovieList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function MovieList() {
+  return <h2>MovieList</h2>;
+}
+
+export default MovieList;
