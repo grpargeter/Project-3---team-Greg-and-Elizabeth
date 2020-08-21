9184c36fcda27308c9fafd5e6648a685f03aed7d
grpargeter <pargetergregr@johndeere.com>
2020-08-21 14:53:05 -0500

    rendering Title and Images on App.js

--- project3/src/App.js
+++ project3/src/App.js
@@ -2,18 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 import MovieList from "./MovieList";
 import MovieDetail from "./MovieDetail";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       movieId: "",
+      initialMovie: [],
+      title: "",
+      poster: "",
     };
   }
+  componentDidMount = () => {
+    this.getMovies();
+  };
+  getMovies = () => {
+    axios
+      .get(`http://www.omdbapi.com/?s=summer&apikey=4d3f7a95&page=1`)
+      .then((response) => {
+        // let movieArray = [response.data.Search];
+        console.log(response.data.Search[0].Poster);
+        this.setState({
+          title: response.data.Search[0].Title,
+          poster: response.data.Search[0].Poster,
+        });
+      });
+  };
   render() {
     return (
       <div className="App">
-        Hello Project 3
+        <h1>{this.state.title}</h1>
+        <img src={this.state.poster} alt="" />
         <MovieList />
         <MovieDetail />
       </div>
