a5dac363b985c428cdd2e7e8c4ae3d8e683b9d13
Elizabeth Freeman <freemanelizabeth@johndeere.com>
2020-08-21 16:02:09 -0500

    end of day commit friday - movie list displaying, some css:

--- project3/src/MovieList.js
+++ project3/src/MovieList.js
@@ -5,14 +5,14 @@ import {Link} from 'react-router-dom'
 const MovieList = props => {
   let movies = props.movieArray.map(movie => {
     return (
-      <div>
-                <h2>{movie.Title}</h2>
-              
+      <div> 
+        <h2>{movie.Title}</h2>
+
 <img src={movie.Poster} alt=""/>
       </div>
     );
   });
-  return <div>{movies}</div>;
+  return <div className = "gallery">{movies}</div>;
 };
 
 export default MovieList;
