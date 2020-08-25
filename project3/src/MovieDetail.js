473a15adbaf07f50155833c03b9aeaffdbab5744
grpargeter <pargetergregr@johndeere.com>
2020-08-25 09:50:49 -0500

    styled Movie list page

--- project3/src/MovieDetail.js
+++ project3/src/MovieDetail.js
@@ -16,13 +16,15 @@ class MovieDetail extends Component {
       plot: "",
       ratings: [],
       poster: "",
-      imdbID: ""
+      imdbID: "",
     };
   }
   componentDidMount = (props) => {
-    console.log(this.props)
+    console.log(this.props);
     axios
-      .get(`http://www.omdbapi.com/?apikey=4d3f7a95&page=1&i=${this.props.match.params.imdbID}`)
+      .get(
+        `http://www.omdbapi.com/?apikey=4d3f7a95&page=1&i=${this.props.match.params.imdbID}`
+      )
       .then((response) => {
         {
           this.setState({
@@ -43,13 +45,17 @@ class MovieDetail extends Component {
   render() {
     return (
       <div className="wholepage">
-         <h1>{this.state.title}</h1> <h4>({this.state.year})</h4> 
-         <h4>Rated {this.state.rated}</h4>  
-         {this.state.genre}<br></br>
-        <img src={this.state.poster} alt=""/>
+        <nav className="homepage">
+          <Link to="/">Movie Home Page</Link>
+        </nav>
+        <h1>{this.state.title}</h1> <h4>({this.state.year})</h4>
+        <h4>Rated {this.state.rated}</h4>
+        {this.state.genre}
+        <br></br>
+        <img src={this.state.poster} alt="" />
         <h4>Plot</h4>
         {this.state.plot}
-         <h4>Actors</h4>
+        <h4>Actors</h4>
         {this.state.actors}
         <h4>Director</h4>
         {this.state.director}
