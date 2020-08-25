6af825cdae6e0b90aa8f4cac3917d1e635831a42
Elizabeth Freeman <freemanelizabeth@johndeere.com>
2020-08-25 10:59:57 -0500

    css updates to movie detail

--- project3/src/MovieDetail.js
+++ project3/src/MovieDetail.js
@@ -45,22 +45,24 @@ class MovieDetail extends Component {
   render() {
     return (
       <div className="wholepage">
-        <nav className="homepage">
+              <nav className="homepage">
           <Link to="/">Movie Home Page</Link>
         </nav>
-        <h1>{this.state.title}</h1> <h4>({this.state.year})</h4>
-        <h4>Rated {this.state.rated}</h4>
-        {this.state.genre}
-        <br></br>
-        <img src={this.state.poster} alt="" />
-        <h4>Plot</h4>
-        {this.state.plot}
-        <h4>Actors</h4>
-        {this.state.actors}
-        <h4>Director</h4>
-        {this.state.director}
+        <div className="header"> <h1>{this.state.title}</h1></div>
+        <div className="content">
+        <div className = "leftside">
+          <img className="poster" src={this.state.poster} alt="" /></div>
+        <div className = "rightside"> 
+        <h4>Year </h4> {this.state.year}
+        <h4>Rated</h4> {this.state.rated}
+        <h4>Genre</h4> {this.state.genre}     
+        <h4>Plot</h4>  {this.state.plot}
+        <h4>Actors</h4> {this.state.actors}
+        <h4>Director</h4> {this.state.director}
         {this.state.reviews}
-      </div>
+        </div>
+        </div>
+        </div>
     );
   }
 }
