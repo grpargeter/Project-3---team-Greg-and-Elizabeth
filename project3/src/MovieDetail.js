a454a7340c4c738836ee2155ac9a6014418d9a48
grpargeter <pargetergregr@johndeere.com>
2020-08-26 15:35:43 -0500

    added ratings

--- project3/src/MovieDetail.js
+++ project3/src/MovieDetail.js
@@ -26,44 +26,59 @@ class MovieDetail extends Component {
         `http://www.omdbapi.com/?apikey=4d3f7a95&page=1&i=${this.props.match.params.imdbID}`
       )
       .then((response) => {
-        {
-          this.setState({
-            title: response.data.Title,
-            year: response.data.Year,
-            rated: response.data.Rated,
-            genre: response.data.Genre,
-            director: response.data.Director,
-            actors: response.data.Actors,
-            plot: response.data.Plot,
-            ratings: response.data.Ratings,
-            poster: response.data.Poster,
-          });
-        }
+        console.log(response.data);
+        let ratings = response.data.Ratings.map((rating) => {
+          return (
+            <h4>
+              {rating.Source} = {rating.Value}
+            </h4>
+          );
+        });
+        this.setState({
+          title: response.data.Title,
+          year: response.data.Year,
+          rated: response.data.Rated,
+          genre: response.data.Genre,
+          director: response.data.Director,
+          actors: response.data.Actors,
+          plot: response.data.Plot,
+          ratings: ratings,
+          poster: response.data.Poster,
+        });
       });
   };
 
   render() {
     return (
       <div className="wholepage">
-              <nav className="homepage">
-          <Link to="/">return to Movie Home Page</Link><br></br>
+        <nav className="homepage">
+          <Link to="/">return to Movie Home Page</Link>
+          <br></br>
           <Link to="/MovieTrivia">check out some movie trivia</Link>
         </nav>
-        <div className="header"> <h2>{this.state.title}</h2></div>
+        <div className="header">
+          {" "}
+          <h2>{this.state.title}</h2>
+        </div>
         <div className="content">
-        <div className = "leftside">
-          <img className="poster" src={this.state.poster} alt="no poster available" /></div>
-        <div className = "rightside"> 
-        <h4>Year </h4> {this.state.year}
-        <h4>Rated</h4> {this.state.rated}
-        <h4>Genre</h4> {this.state.genre}     
-        <h4>Plot</h4>  {this.state.plot}
-        <h4>Actors</h4> {this.state.actors}
-        <h4>Director</h4> {this.state.director}
-        {this.state.reviews}
+          <div className="leftside">
+            <img
+              className="poster"
+              src={this.state.poster}
+              alt="no poster available"
+            />
+          </div>
+          <div className="rightside">
+            <h4>Year: {this.state.year}</h4>
+            <h4>Rated: {this.state.rated}</h4>
+            <h4>Genre: {this.state.genre}</h4>
+            <h4>Plot: {this.state.plot}</h4>
+            <h4>Actors: {this.state.actors}</h4>
+            <h4>Director: {this.state.director}</h4>
+            {this.state.ratings}
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     );
   }
 }
